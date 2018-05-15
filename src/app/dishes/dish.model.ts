332b18bc413d082709d348061e8f96015ea76d01
Konstantin Kuzmin <kocc95@mail.ru>
2018-05-15 20:50:41 +0200

    added description attribute to Dish model

--- src/app/dishes/dish.model.ts
+++ src/app/dishes/dish.model.ts
@@ -3,13 +3,16 @@ export class Dish {
   name: string;
   price: number;
   imagePath: string;
+  description: string;
   categoryId: number;
 
-  constructor(id: number, name: string, price: number, imagePath: string, categoryId: number) {
+
+  constructor(id: number, name: string, price: number, imagePath: string, description: string, categoryId: number) {
     this.id = id;
     this.name = name;
     this.price = price;
     this.imagePath = imagePath;
+    this.description = description;
     this.categoryId = categoryId;
   }
 }
