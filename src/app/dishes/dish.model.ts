a7f6dab2f5eba65416e53cc789e174ef7456f61a
Konstantin <kosax22@gmail.com>
2018-05-30 15:29:08 +0200

    Dish model extended

--- src/app/dishes/dish.model.ts
+++ src/app/dishes/dish.model.ts
@@ -1,8 +1,12 @@
+import {Ingredient} from "../ingredients/ingredient.model";
+
 export interface Dish {
   id: number;
   name: string;
   price: number;
   imagePath: string;
   description: string;
+  purchases;
+  ingredients: Ingredient[];
   categoryId: number;
 }
