9123b557a784f97c8ab0d45c0c21483cec1f0717
Konstantin <kocc95@mail.ru>
2018-06-07 22:56:46 +0200

    added possibility to add dish into basket

--- src/app/dishes/dish-detail/dish-detail.component.ts
+++ src/app/dishes/dish-detail/dish-detail.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Dish} from '../dish.model';
 import {DishService} from '../dish.service';
 import {ActivatedRoute, Params, Router} from '@angular/router';
+import {PurchaseService} from '../../purchase/purchase.service';
 
 @Component({
   selector: 'app-dish-detail',
@@ -13,23 +14,29 @@ export class DishDetailComponent implements OnInit, OnDestroy {
   dish: Dish;
   id: number;
   subscription;
+  dishes: Dish[];
 
   constructor(private dishService: DishService,
               private router: Router,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private purchaseService: PurchaseService) {
   }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
+        this.subscription = this.dishService.getDishes().subscribe(
+          dishes => {
+            this.dish = dishes[this.id];
+          }
+        );
       }
     );
-    this.subscription = this.dishService.getDishes().subscribe(
-      dishes => {
-        this.dish = dishes[this.id]
-      }
-    )
+  }
+
+  onAddDish() {
+    this.purchaseService.addProduct({name: this.dish.name, price: this.dish.price});
   }
 
   ngOnDestroy() {
